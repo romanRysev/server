b8b6b654bbaa5081ffb14e11ebd88e841cc9a38c
Roman Rysev <roman.rysev@gmail.com>
2020-05-25 22:54:40 +0300

    debug

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 module.exports = {
   DATABASE_URL: 'mongodb://localhost:27017/mestodb',
-  PORT: parseInt(process.env.PORT) || 3000,
+  PORT: 3000,
   JWT_SECRET: process.env.JWT_SECRET || 'JWT_SECRET',
 };
